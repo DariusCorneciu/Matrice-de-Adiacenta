47f976c2ea46878e6d8978d420ff5737f50ebe06
Darius Corneciu <116907008+DariusCorneciu@users.noreply.github.com>
2022-12-30 16:46:19 +0200

    Cerinta 1 si 2 proiect ASC

--- Corneciu_DariusStefan_0.s
+++ Corneciu_DariusStefan_0.s
@@ -7,6 +7,7 @@
 	mres: .space 40000
 	scan: .asciz "%ld\n"
 	print: .asciz "%ld "
+	pcer: .asciz "Numarul de drumuri de lungime l este %ld \n"
 	endl: .asciz "\n"
 	cerinta: .space 4
 	n: .space 4
@@ -26,10 +27,10 @@
 matrix_mult:
 	pushl %ebp
 	mov %esp,%ebp
-	push %ebx
-	push %edi
-	push %edx
-	push %ecx
+	pushl %ebx
+	pushl %edi
+	pushl %edx
+	pushl %ecx
 	subl $24,%esp
 	
 	mov 8(%ebp),%ebx
@@ -37,6 +38,7 @@ matrix_mult:
 	mov 16(%ebp),%edx 
 	mov 20(%ebp),%ecx
 	
+	movl $0,-20(%ebp)
 	
 	for_iii:
 		cmp -20(%ebp),%ecx
@@ -50,13 +52,11 @@ matrix_mult:
 				cmp -28(%ebp),%ecx
 				je jpp
 				
-				
 				movl -20(%ebp),%eax
 				movl $0,%edx
 				mull %ecx
 				addl -28(%ebp),%eax
 				movl %eax,-32(%ebp)
-				
 				
 				movl -28(%ebp),%eax
 				movl $0,%edx
@@ -64,27 +64,26 @@ matrix_mult:
 				addl -24(%ebp),%eax
 				movl %eax,-36(%ebp)
 				
-				movl -32(%ebp),%edx
-				movl (%ebx,%edx,4),%eax
-				movl $0,%edx
-				movl -36(%ebp),%ecx
-				mull (%edi,%ecx,4)
-				movl 20(%ebp),%ecx
-				movl %eax,-40(%ebp)
-				
 				movl -20(%ebp),%eax
 				movl $0,%edx
 				mull %ecx
 				addl -24(%ebp),%eax
 				movl %eax,-44(%ebp)
 				
-				mov 16(%ebp),%edx
+				movl -32(%ebp),%edx
+				movl (%ebx,%edx,4),%eax
+				movl -36(%ebp),%edx
+				movl (%edi,%edx,4),%ecx
+				movl $0,%edx
+				mull %ecx
 				
-				movl -44(%ebp),%ebx
-				movl (%edx,%ebx,4),%eax
-				addl -40(%ebp),%eax
-				movl %eax,(%edx,%ebx,4)
-				mov 8(%ebp),%ebx
+				mov 16(%ebp),%edx 
+				
+				movl -44(%ebp),%ecx
+				addl %eax,(%edx,%ecx,4)
+				
+				mov 20(%ebp),%ecx
+				
 				
 				incl -28(%ebp)
 				jmp for_kkk
@@ -98,13 +97,15 @@ matrix_mult:
 		incl -20(%ebp)
 		jmp for_iii
 	
+	
 	pro_exit:
 	addl $24,%esp
-	pop %ecx
-	pop %edx
-	pop %edi
-	pop %ebx
-	pop %ebp
+	popl %ecx
+	popl %edx
+	popl %edi
+	popl %ebx
+	popl %ebp
+	ret
 main:
 	pushl $cerinta
 	pushl $scan
@@ -269,6 +270,15 @@ cerinta2:
 	
 	movl lun,%eax
 	sub $1,%eax
+	movl %eax,lun
+	push n
+	push $mres
+	push $m1
+	push $m1
+	call matrix_mult
+	addl $16,%esp
+	
+	movl $1,index
 	for_ii:
 		movl index,%ebx
 		cmp %ebx,lun
@@ -277,12 +287,12 @@ cerinta2:
 		for_jj:
 			movl jndex,%ebx
 			cmp %ebx,n
-			je creste_i
+			je matmul
 			movl $0,kndex
 			for_kk:
 				movl kndex,%ebx
 				cmp %ebx,n
-				je multire
+				je cre_j
 				
 				lea mres,%edi
 				movl jndex,%eax
@@ -296,15 +306,26 @@ cerinta2:
 				
 				incl kndex
 				jmp for_kk
-			multire:
+			cre_j:
 			incl jndex
 			jmp for_jj
 
-	creste_i:
+	matmul:
+	push n
+	push $mres
+	push $m2
+	push $m1
+	call matrix_mult
+	addl $16,%esp	
 		incl index
 		jmp for_ii
-		
+	
+	
 	afis2:
+		pushl $0
+		call fflush
+		pop %ebx
+		
 		lea mres,%edi
 		movl ns,%eax
 		movl $0,%edx
@@ -313,7 +334,7 @@ cerinta2:
 		movl (%edi,%eax,4),%ebx
 		
 		push %ebx
-		push $print
+		push $pcer
 		call printf
 		addl $8,%esp
 		
